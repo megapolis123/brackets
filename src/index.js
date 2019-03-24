16566d3bdd97e8b6ec7545c9ca336887223043c9
megapolis123 <al.averin123@gmail.com>
2019-03-24 12:11:15 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let array = str.split('');
+
+  for( let i = 0; i < array.length; i++) {
+    for( let j = 0; j < bracketsConfig.length; j++) {
+      if( array[i] == bracketsConfig[j][0] && array[i+1] == bracketsConfig[j][1]) {
+        array.splice(i,2);
+        i = i - 2;
+      }
+    }
+  }
+
+  if(array.length == 0){
+    return true;
+  }else {
+    return false;
+}
 }
